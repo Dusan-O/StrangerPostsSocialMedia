060aded4160f9de46dfc416a1067a64278b0c2d9
Dusan-O <dusan.orescanin@gmail.com>
2022-08-31 12:47:59 +0200

    Initial Commit

---
+++ StrangerPostsSocialMedia/StrangerPostsSocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrangerPostsSocialMediaApp.swift
+//  StrangerPostsSocialMedia
+//
+//  Created by Dusan Orescanin on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct StrangerPostsSocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
